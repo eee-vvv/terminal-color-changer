4a61336e4249fd08af37cd84c9cbfe93200845cd
eeevvv <evan.w.and@gmail.com>
2022-05-06 06:08:46 -0700

    loop over prompt until correct entry is made

--- color-changer.sh
+++ color-changer.sh
@@ -11,26 +11,30 @@
 ######################################################################################################
 
 echo "Please pick a colorscheme from the following."
-echo ""
+echo
 echo "Enter it at the command prompt below and"
 echo "press enter."
-echo ""
+echo
+
 # find and print all available color schemes from alacritty.yml
 sed -nr 's/.*: &(.*)$/\1/p' ~/.config/alacritty/alacritty.yml | tee /tmp/temp_color.txt
-echo ""
-echo -n "> "
-read choice
+echo
 
-if grep -Fxq "$choice" /tmp/temp_color.txt
-then
-  # code if found
-  alacritty_replacement="colors: \*$choice"
-  nvim_replacement="colorscheme $choice"
-  sed -i .bak "s/^colors:.*/$alacritty_replacement/" ~/.config/alacritty/alacritty.yml
-  sed -i .bak "s/^colorscheme.*/$nvim_replacement/" ~/.config/nvim/color.vim
-else
-  echo ""
-  echo "Choice not valid. Please try again."
-fi
+while true; do
+  echo -n "> "
+  read choice
+
+  if grep -Fxq "$choice" /tmp/temp_color.txt
+  then
+    # if found, replace respective lines of alacritty.yml and color.vim
+    alacritty_replacement="colors: \*$choice"
+    nvim_replacement="colorscheme $choice"
+    sed -i .bak "s/^colors:.*/$alacritty_replacement/" ~/.config/alacritty/alacritty.yml
+    sed -i .bak "s/^colorscheme.*/$nvim_replacement/" ~/.config/nvim/color.vim
+    break
+  else
+    echo "Choice not valid. Please try again."
+  fi
+done
 
 rm /tmp/temp_color.txt
