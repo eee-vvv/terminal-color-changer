3f96e310815b08325fbbe1f945c3ecbc8f59b54d
eeevvv <evan.w.and@gmail.com>
2022-05-06 06:49:48 -0700

    script can optionally take one argument

--- color-changer.sh
+++ color-changer.sh
@@ -9,6 +9,29 @@
 # Right now, only guaranteed compatible with Apple OS
 # TODO: test on my arch machine
 
+check_matched () {
+  if grep -Fxq "$1" /tmp/temp_color.txt; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+sed -nr 's/.*: &(.*)$/\1/p' ~/.config/alacritty/alacritty.yml > /tmp/temp_color.txt
+matched=false
+
+if [ $# -eq 1 ]; then
+  if check_matched $1; then
+    matched=true
+    choice=$1
+  else
+    echo "Invalid argument supplied"
+    exit 1
+  fi
+elif [ $# -gt 1 ]; then
+  echo "Too many arguments supplied"
+  exit 1
+fi
 
 echo "Please pick a colorscheme from the following."
 echo
@@ -17,24 +40,21 @@ echo "press enter."
 echo
 
 # find and print all available color schemes from alacritty.yml
-sed -nr 's/.*: &(.*)$/\1/p' ~/.config/alacritty/alacritty.yml | tee /tmp/temp_color.txt
+cat /tmp/temp_color.txt
 echo
 
-while true; do
+while [ $matched = false ]; do
   echo -n "> "
   read choice
 
-  if grep -Fxq "$choice" /tmp/temp_color.txt
-  then
-    # if found, replace respective lines of alacritty.yml and color.vim
-    alacritty_replacement="colors: \*$choice"
-    nvim_replacement="colorscheme $choice"
-    sed -i .bak "s/^colors:.*/$alacritty_replacement/" ~/.config/alacritty/alacritty.yml
-    sed -i .bak "s/^colorscheme.*/$nvim_replacement/" ~/.config/nvim/color.vim
-    break
+  if check_matched $choice; then
+    matched=true
   else
     echo "Choice not valid. Please try again."
   fi
 done
 
-rm /tmp/temp_color.txt
+alacritty_replacement="colors: \*$choice"
+nvim_replacement="colorscheme $choice"
+sed -i .bak "s/^colors:.*/$alacritty_replacement/" ~/.config/alacritty/alacritty.yml
+sed -i .bak "s/^colorscheme.*/$nvim_replacement/" ~/.config/nvim/color.vim
